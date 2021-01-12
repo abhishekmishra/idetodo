e370be1905606e4264032c4a1a2065b7c51d6967
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-01-12 20:16:55 +0530

    added workflow definition tentative

---
+++ workflow.lua
@@ -0,0 +1,21 @@
+{
+	name = "workflow to do something",
+	description = "blah blah blah",
+	projects = {}
+	contexts = {}
+	tasks = {
+			{
+				task = "task0",
+				checklist = {
+					check thing,
+					check another thing,
+					check yet another thing,
+				},
+				metrics = {
+					metric1: integer,
+					metric2: numeric
+				},
+				next = value or a lambda
+			}
+		}
+	}
