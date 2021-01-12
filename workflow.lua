92695b5bc4fb729c2cbfc295298d6320049aadf4
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-01-12 23:02:42 +0530

    fixed syntax errors

--- workflow.lua
+++ workflow.lua
@@ -1,21 +1,21 @@
-{
+workflow = {
 	name = "workflow to do something",
 	description = "blah blah blah",
-	projects = {}
-	contexts = {}
+	projects = {},
+	contexts = {},
 	tasks = {
 			{
 				task = "task0",
 				checklist = {
-					check thing,
-					check another thing,
-					check yet another thing,
+					"check thing",
+					"check another thing",
+					"check yet another thing",
 				},
 				metrics = {
-					metric1: integer,
-					metric2: numeric
+					metric1= "integer",
+					metric2= "numeric"
 				},
-				next = value or a lambda
+				next = "value or a lambda"
 			}
 		}
 	}
