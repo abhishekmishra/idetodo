ae7c76f43991287283b26124830128cb106cafdd
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-01-16 18:34:27 +0530

    directly invoking python from command entry

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
-_CONFIG_FILE = "idetodo.cfg"
+import configparser
+
+_CONFIG_FILE = "idetodo.ini"
 
 
-def read_config(lua):
-    # print(lua.execute('''
-    #     x = 10
-    #     print(x)
-    #     return x
-    # '''))
-
-    cfg = None
-
-    with open(_CONFIG_FILE, 'r') as f:
-        cfg_text = f.read()
-        cfg = lua.execute(cfg_text)
-        # print(cfg.items())
-        # print(cfg["theme"])
-    return cfg
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(_CONFIG_FILE)
+    return config
--- idetodo.py
+++ idetodo.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import os
 import sys
 from view_calendar import weekly_agenda
-from lupa import LuaRuntime, LuaSyntaxError, LuaError
 from datetime import date, timedelta
 
 win_values = None
@@ -16,7 +15,7 @@ todos = None
 def todo_ask():
     todo_text = sg.PopupGetText("Add todo:", "Add todo")
     if todo_text:
-        lualine_eval_print("todo('" + todo_text + "')")
+        pyline_eval_print("todo('" + todo_text + "')")
         return todo_text
     else:
         return "cancelled"
@@ -100,42 +99,12 @@ def agenda():
     weekly_agenda(todos)
 
 
-lua = LuaRuntime(unpack_returned_tuples=True)
-lua.execute("""
-function pyfunc(name)
-    return python.eval(name)
-end
-
-todo = pyfunc("todo")
-done = pyfunc("done")
-daily_todo=pyfunc("daily_todo")
-todo_ask = pyfunc("todo_ask")
-update_ask = pyfunc("update_ask")
-save = pyfunc("save")
-reload = pyfunc("reload")
-selected = pyfunc("selected")
-debug = pyfunc("sg.Print")
-popup_ok = pyfunc("sg.popup_ok")
-agenda = pyfunc("agenda")
-
-""")
-
-
-def update_lua_py_globals():
-    lua.execute("""
-    -- python variables
-    todos = python.eval("todos")
-    win_event = python.eval("win_event")
-    win_values = python.eval("win_values")
-    """)
-
-
-cfg = read_config(lua)
-sg.theme(cfg["theme"])
+cfg = read_config()
+sg.theme(cfg["ui"]["theme"])
 
 HOME_DIR = str(Path.home())
-TODO_DIR = os.path.join(HOME_DIR, cfg["todo_dir"])
-TODO_TXT_PATH = os.path.join(TODO_DIR, cfg["todo_file"])
+TODO_DIR = os.path.join(HOME_DIR, cfg["todotxt"]["todo_dir"])
+TODO_TXT_PATH = os.path.join(TODO_DIR, cfg["todotxt"]["todo_file"])
 
 
 # ------ Menu Definition ------ #
@@ -160,36 +129,26 @@ menu_def = [
     ['Report', ['Daily']],
     ['Help', 'About'], ]
 
-lualine_history = []
-lualine_count = 0
+pyline_history = []
+pyline_count = 0
 
 
-def lualine_eval_print(lua_input):
+def pyline_eval_print(py_input):
     # print read value
-    window['-LUAOUTPUT-'].print('> ', lua_input)
-    global lualine_count
-    lualine_count = lualine_count + 1
-    lualine_history.append(lua_input)
+    window['-PYOUTPUT-'].print('> ', py_input)
+    global pyline_count
+    pyline_count = pyline_count + 1
+    pyline_history.append(py_input)
     # eval
-    lua_output = None
+    py_output = None
     error = True
     try:
-        lua_output = lua.eval(lua_input)
+        py_output = eval(py_input)
         error = False
-    except LuaSyntaxError as syntax_error:
-        try:
-            lua_output = lua.execute(lua_input)
-            error = False
-        except LuaSyntaxError as syntax_error_2:
-            lua_output = syntax_error
-        except LuaError as lua_error:
-            lua_output = lua_error
-    except LuaError as lua_error:
-        lua_output = lua_error
     except:
-        lua_output = "Unexpected error:", sys.exc_info()[0]
+        py_output = "Unexpected error:", sys.exc_info()[0]
     # print
-    window['-LUAOUTPUT-'].print(str(lua_output), text_color='red' if error else 'green')
+    window['-PYOUTPUT-'].print(str(py_output), text_color='red' if error else 'green')
 
 
 if __name__ == '__main__':
@@ -215,11 +174,11 @@ if __name__ == '__main__':
         [sg.Listbox(key="-TODOLIST-", values=todos.ls,
                     default_values=[selected_todo], size=(LAYOUT_WIDTH, 30), enable_events=True, auto_size_text=True)],
         # [sg.Button("Quit")],
-        [sg.Multiline(default_text="", key="-LUAOUTPUT-", size=(LAYOUT_WIDTH, 5),
+        [sg.Multiline(default_text="", key="-PYOUTPUT-", size=(LAYOUT_WIDTH, 5),
                       disabled=True, autoscroll=True)],
         [sg.T(text="IDETODO >>", size=(10, None)),
-         sg.Input(default_text="", key="-LUALINE-", size=(LAYOUT_WIDTH - 11, None), enable_events=True, focus=True),
-         sg.Button('-SUBMIT_LUALINE-', visible=False, bind_return_key=True)],
+         sg.Input(default_text="", key="-PYLINE-", size=(LAYOUT_WIDTH - 11, None), enable_events=True, focus=True),
+         sg.Button('-SUBMIT_PYLINE-', visible=False, bind_return_key=True)],
         [sg.StatusBar("Filter", key="-STATUSBAR_FILTER-"), sg.StatusBar("Sort", key="-STATUSBAR_SORT-"),
          sg.StatusBar("Tasks", key="-STATUSBAR_TASKS-"), sg.StatusBar("Incomplete", key="-STATUSBAR_INCOMPLETE-"),
          sg.StatusBar("Due Today", key="-STATUSBAR_DUE_TODAY-"), sg.StatusBar("Overdue", key="-STATUSBAR_OVERDUE-")]
@@ -230,42 +189,41 @@ if __name__ == '__main__':
     # see docs - persistent window - multiple reads using an event loop
     while True:
         win_event, win_values = window.read()
-        update_lua_py_globals()
 
         if win_event == sg.WIN_CLOSED or win_event == 'Quit':
             break
 
         if win_event == 'Up:38':
-            lualine_count -= 1
-            if -1 < lualine_count < len(lualine_history):
-                window['-LUALINE-'].update(lualine_history[lualine_count])
+            pyline_count -= 1
+            if -1 < pyline_count < len(pyline_history):
+                window['-PYLINE-'].update(pyline_history[pyline_count])
 
         if win_event == 'Down:40':
-            lualine_count += 1
-            if -1 < lualine_count < len(lualine_history):
-                window['-LUALINE-'].update(lualine_history[lualine_count])
+            pyline_count += 1
+            if -1 < pyline_count < len(pyline_history):
+                window['-PYLINE-'].update(pyline_history[pyline_count])
 
         if win_event in (get_menu_key(task_new), 'n:78', 'n:57'):
-            lualine_eval_print("todo_ask()")
+            pyline_eval_print("todo_ask()")
 
         if win_event in (get_menu_key(task_update), 'u:85'):
-            lualine_eval_print("update_ask()")
+            pyline_eval_print("update_ask()")
 
         if win_event in (get_menu_key(file_reload), 'period:190'):
-            lualine_eval_print("reload()")
+            pyline_eval_print("reload()")
 
         if win_event in (get_menu_key(file_save), 's:83'):
-            lualine_eval_print("save()")
+            pyline_eval_print("save()")
 
         if win_event in (get_menu_key(task_done), 'x:88', 'x:53'):
-            lualine_eval_print("done()")
+            pyline_eval_print("done()")
 
-        if win_event == '-SUBMIT_LUALINE-':
+        if win_event == '-SUBMIT_PYLINE-':
             # read
-            lua_line = win_values['-LUALINE-']
-            lualine_eval_print(lua_line)
+            lua_line = win_values['-PYLINE-']
+            pyline_eval_print(lua_line)
             # loop
-            window['-LUALINE-'].update("")
+            window['-PYLINE-'].update("")
 
         if win_event == 'Daily':
             agenda()
